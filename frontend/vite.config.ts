2d93230adddd5261a2ad359cde784fe6b1dc15b6
xyqlx <saint.diana@outlook.com>
2024-06-20 00:03:17 +0800

    🚧create and init frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    hmr: {
+      path: '/__vite_hmr',
+      clientPort: 443,
+    },
+    watch: {
+      usePolling: true,
+    }
+  }
+})
