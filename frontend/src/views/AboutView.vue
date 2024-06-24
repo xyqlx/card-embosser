365e2946cff1b2ee44f08c2a7b6b8ac231a11d59
xyqlx <saint.diana@outlook.com>
2024-06-24 20:13:23 +0800

    💄modify about page and title

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>录入你的所有物</h1>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  font-size: 2em;
+  text-align: center;
 }
 </style>
